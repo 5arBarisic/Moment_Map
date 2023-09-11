3cb716430f53fe77fec20d8e320c82b7c9765cc2
Petar Barisic <pbarisic@croz.net>
2023-09-11 22:37:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/momentmap/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.momentmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.momentmap.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView ) as NavHostFragment
+        navController=navHostFragment.navController
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.botNav)
+        setupWithNavController(bottomNavigationView,navController)
+    }
+
+}
---
+++ app/src/main/java/com/example/momentmap/Moment.kt
@@ -0,0 +1,9 @@
+package com.example.momentmap
+
+data class Moment(val title: String? = null,
+                  val description: String? = null,
+                  val location: String? = null,
+                  val date: String? = null ,
+                  val imageUrl: String? = null ,
+    )
+
